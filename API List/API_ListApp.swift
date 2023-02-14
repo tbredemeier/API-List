f76fe4cdf50f260d15642190d26bedb1cfbe41e6
Tom Bredemeier <tbredemeier@barrington220.org>
2023-02-14 14:06:10 -0600

    Initial Commit

---
+++ API List/API_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_ListApp.swift
+//  API List
+//
+//  Created by Tom Bredemeier on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct API_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
